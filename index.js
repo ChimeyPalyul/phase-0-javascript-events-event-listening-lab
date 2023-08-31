63f0b7f6ff7d282657b88d80e86c463a962d4da6
Chimey Palyul <palyul.chimey@gmail.com>
2023-08-31 11:39:00 -0600

    addEventListener

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const input= document.getElementById('button');
+
+function clickAlert(){
+    alert("Surprise pal")
+}
 function addingEventListener() {
+    input.addEventListener('click',clickAlert);
 }
